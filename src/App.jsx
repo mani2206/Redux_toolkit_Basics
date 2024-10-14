d5ab68fa0a43b5cdf2af5a69b6f4c53d3dfec26c
Manikandan.S <94900129+mani2206@users.noreply.github.com>
2024-10-14 02:55:26 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Counter from './feature/counter/Counter'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline' >Welcome Mani</h1>
+      <Counter />
+    </>
+  )
+}
+
+export default App
